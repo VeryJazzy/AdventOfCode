6ef4d315bbc864bde61abcdf1f52cc4e12310ac6
very jazzy <jackwilliamcastle@outlook.com>
2020-12-18 10:58:51 +0000

    day 6 part 2 complete

--- src/Day6Input/Main.java
+++ src/Day6Input/Main.java
@@ -2,8 +2,7 @@ package Day6Input;
 
 import Utility.FileUtil;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,12 +14,17 @@ public class Main {
         String puzzleInput = FileUtil.readFile("C:\\Users\\VeryJazzy\\Desktop\\adventCode1\\Resources\\Day6Input.txt");
         String[] groups = puzzleInput.split("\r\n\r\n");
 
+        part1(groups);
+
+        part2(groups);
+    }
+
+    public void part1(String[] groups) {
         int sumOfYeses = 0;
         for (String group : groups) {
             sumOfYeses += countYeses(group);
         }
         System.out.println(sumOfYeses);
-
 
     }
 
@@ -33,4 +37,74 @@ public class Main {
         }
         return uniqueChars.size();
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void part2(String[] groups) {
+        int sumOfYeses = 0;
+
+        for (String group : groups) {
+            sumOfYeses += countIfEveryoneSaysYesToAQuestion(group);
+        }
+        System.out.println(sumOfYeses);
+    }
+
+
+    public int countIfEveryoneSaysYesToAQuestion(String group) {
+        String[] people = group.split("\r\n");
+        HashSet<Character> charsToCompare = new HashSet<>(stringToCharArrList(people[0]));
+
+        for (String person : people) {
+             ArrayList<Character> personArrList = stringToCharArrList(person);
+             ArrayList<Character> toRemove = new ArrayList<Character>();
+             for (char c : charsToCompare) {
+                 if (!personArrList.contains(c)) {
+                     toRemove.add(c);
+                 }
+             }
+             charsToCompare.removeAll(toRemove);
+        }
+
+        return charsToCompare.size();
+    }
+
+    public ArrayList<Character> stringToCharArrList(String s) {
+        ArrayList<Character> arrList = new ArrayList<Character>();
+        for (char c : s.toCharArray()) {
+            arrList.add(c);
+        }
+        return arrList;
+    }
+
+
+
+
+
+
+
 }
